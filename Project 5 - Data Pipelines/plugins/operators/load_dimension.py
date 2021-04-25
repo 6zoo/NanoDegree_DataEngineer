efe656e01b86e2d233658712ae13e433b1cc8ee1
Terry, Jung <54563618+6zoo@users.noreply.github.com>
2021-04-25 23:36:32 +0900

    upload files

---
+++ Project 5 - Data Pipelines/plugins/operators/load_dimension.py
@@ -0,0 +1,48 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadDimensionOperator(BaseOperator):
+    """
+    Loads dimension table in Redshift from staging table
+    :param redshift_conn_id: Redshift connection ID set in Airflow connection
+    :param table: Target table in Redshift to load
+    :param select_sql: SQL query to get data into target table
+    :param delete_load : default False. Able to switch between append-only and delete-load functionality
+    :param sql_source : SQL query for getting data to load into target table
+    """
+    ui_color = '#80BD9E'
+
+    insert_sql = """
+        INSERT INTO {}
+        {}
+        ;
+    """
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 table="",
+                 delete_load=False,
+                 sql_source="",
+                 *args, **kwargs):
+
+        super(LoadDimensionOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.delete_load = delete_load
+        self.sql_source = sql_source
+
+    def execute(self, context):
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        if self.delete_load:
+            self.log.info("Truncating Redshift table")
+            redshift.run("DELETE FROM {}".format(self.table))
+
+        formatted_sql = LoadDimensionOperator.insert_sql.format(
+            self.table,
+            self.sql_source
+        )
+        self.log.info(f"Executing {formatted_sql} ...")
+        redshift.run(formatted_sql)
